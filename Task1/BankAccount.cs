698748a1be0e43aa68fa9abbf681bf3ad1911c60
Anish-Programmer <at7anish@gmail.com>
2025-11-26 11:57:18 +0545

    Task 1 completed

---
+++ Task1/BankAccount.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    internal class BankAccount
+    {
+        private string accountNumber;
+        private double balance;
+
+        // constructor
+        public BankAccount(string accountNumber, double initialBalance) 
+        { 
+            this.accountNumber = accountNumber;
+
+            // condtion
+            if (initialBalance <= 0) 
+            {
+               
+                throw new ArgumentException("Inital balance must be greater than 0.");
+                //Console.WriteLine("Inital balance must be greater than 0.");
+            }
+
+
+            balance = initialBalance;
+        }
+
+
+        // read-only Account Number property
+        public string AccountNumber
+        {
+            get { return accountNumber; }
+        }
+
+        // balance with private setter
+        public double Balance
+        { 
+            get { return balance; }
+            private set 
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Balance cannot be zero or negative.");
+                }
+
+                balance = value;
+            }
+        }
+
+
+        // Deposit method
+        public void Deposit(double amount)
+        {
+            if (amount <= 0) 
+            { 
+                Console.WriteLine("Deposit amount must be greater than 0.");
+                return;
+            }
+
+            Balance += amount; // using private setter
+            Console.WriteLine($"Deposited: {amount}");
+        }
+
+
+
+
+
+        // withdraw method
+        public void Withdraw(double amount)
+        {
+            if (amount <= 0) 
+            {
+                Console.WriteLine("Withdraw amount must be greater than zero.");
+                return;
+            }
+
+
+            if (amount > Balance)
+            { 
+                Console.WriteLine("Insufficient balance!.");
+                return;
+            }
+
+            Balance -= amount; // using private setter
+            Console.WriteLine($"Withdrew: {amount}");
+        }
+    }
+}
--- Task1/Program.cs
+++ Task1/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Creating an BankAccount object
+            BankAccount account = new BankAccount("ACC11", 7000);
+
+            Console.WriteLine($"Account Number: {account.AccountNumber}");
+            Console.WriteLine($"Initial Balance: {account.Balance}");
+
+            // Depositing money
+            account.Deposit(2000);
+
+            // withdrawing money
+            account.Withdraw(1000);
+
+            // printing final balance
+            Console.WriteLine("Remaining Balance: "+ account.Balance);
+
+            Console.ReadLine();
         }
     }
 }
